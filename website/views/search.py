0c85e48f2b72b424a1782119d5835ea428fd93d0
Marek <marekwypich@gmail.com>
2022-11-24 00:14:07 +0100

    history_post finished, history_get init

--- website/views/search.py
+++ website/views/search.py
@@ -5,6 +5,9 @@ from ..models import History, Product
 from ..search_engine.product import Product
 from ..search_engine.shop import Shop
 from ..search_engine.json_encoder import CustomEncoder
+from flask_wtf import FlaskForm
+from ..models import History, Shop as ShopModel, Product as ProductModel
+from ..app import db
 
 search = Blueprint('search', __name__)
 
@@ -69,12 +72,41 @@ def search_get():
 
 
 # search history
-# TODO: login_required
-@search.route('/history', methods=['GET'])
-def history_get():
+@search.route('/history/<int:history_id>', methods=['GET'])
+@login_required
+def history_get(history_id):
+    history = History.query.get_or_404(history_id)
+    if history.user_id == current_user.id:
+        pass
+    else:
+        return '', 403
     return 'History', 200
 
 
+@search.route('/history', methods=['POST'])
+@login_required
+def history_post():
+    products_list = request.get_json()
+    history = History(user_id=current_user.id)
+    db.session.add(history)
+    for productJSON in products_list:
+        print(productJSON)
+        product_object = Product(**productJSON)
+        product = ProductModel(history_id=history.id, name=product_object.name, url=product_object.url,
+                               img=product_object.img, description=product_object.description,
+                               rating=product_object.rating)
+        db.session.add(product)
+        for shopJSON in product_object.shop_list:
+            shop_object = Shop(**shopJSON)
+            shop = ShopModel(product_id=product.id, name=shop_object.name, url=shop_object.url,
+                             price=shop_object.price, delivery_price=shop_object.delivery_price,
+                             availability=shop_object.availability, delivery_time=shop_object.delivery_time)
+            db.session.add(shop)
+    db.session.commit()
+    return '', 200
+
+
 @search.route('/history/<int:id>', methods=['DELETE'])
+@login_required
 def history_delete(id):
     return id, 200
