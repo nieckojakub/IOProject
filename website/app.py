8e2b5746bff85269a653e35d251e99a3822b468b
nieckojakub <niecko.jakub@gmail.com>
2022-11-23 19:27:28 +0100

    delete one line

--- website/app.py
+++ website/app.py
@@ -5,7 +5,7 @@ from os import path
 import os
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-from flask_mail import Mail
+
 
 
 db = SQLAlchemy()
@@ -17,7 +17,7 @@ login_manager = LoginManager(app)
 bcrypt = Bcrypt(app)
 login_manager.login_view = 'auth.login'
 login_manager.login_message_category = 'info'
-mail = Mail(app)
+
 
 def create_app():
     bootstrap = Bootstrap(app)
