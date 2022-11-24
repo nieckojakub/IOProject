bebaac7edacf4c09d862901d9251c2dd91bff179
Jakub Szczudlik <jszczudlik@student.agh.edu.pl>
2022-11-24 18:24:48 +0100

    home v2. adding new products works

---
+++ website/static/js/home_scripts.js
@@ -0,0 +1,45 @@
+// variables
+var listToReturn = [];
+//input formu
+var addButton = document.getElementById("addButton");
+var searchInput = document.getElementById("SearchInput");
+var allegroCheckbox = document.getElementById("AllegroCheckbox");
+var ceneoCheckbox = document.getElementById("CeneoCheckbox");
+var bothCheckbox = document.getElementById("BothCheckbox");
+//list
+var listOfProducts = document.getElementById("listOfProducts");
+
+
+
+//function- adding name of the product to the list
+function addProduct(){
+
+    listToReturn.push([searchInput.value, allegroCheckbox.checked, ceneoCheckbox.checked, bothCheckbox.checked]); //nazwa, allegro, cene, both
+    //checking if input is nonempty
+    if(searchInput.value == ""){
+        alert("Please, enter your product first");
+        return 0;
+    }
+    //creating html elements
+    var newProduct = document.createElement("div");
+    var nameDiv = document.createElement("div");
+    var delateBtnDiv = document.createElement("div");
+    var name = document.createElement('h6');
+    var delBtn = document.createElement('button');
+    //setting attributes
+    newProduct.setAttribute("class", "row mb-2 border-bottom border-top border-3");
+    name.innerHTML = searchInput.value;
+    nameDiv.setAttribute("class", "col");
+    delateBtnDiv.setAttribute("class", "col-auto");
+    delBtn.setAttribute("class", "btn btn-danger");
+    delBtn.setAttribute("type", "button");
+    delBtn.innerHTML = "Delete";
+    //connecting elements
+    nameDiv.appendChild(name);
+    delateBtnDiv.appendChild(delBtn);
+    newProduct.appendChild(nameDiv);
+    newProduct.appendChild(delateBtnDiv);
+    //adding new product to the list
+    listOfProducts.appendChild(newProduct);
+    
+}
