8a9750b01bbf66f215a25dc6464b871f34a09857
Marek <marekwypich@gmail.com>
2022-12-08 12:13:10 +0100

    results view

--- website/views/views.py
+++ website/views/views.py
@@ -2,6 +2,12 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/')
 def home():
     return render_template('home.html')
+
+
+@views.route("/results/<token>")
+def results():
+    return render_template('temp.html')
