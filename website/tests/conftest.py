a8f70ddb9d6eae32011387d974fa9eb82339b93b
Hubert Biros <biros@student.agh.edu.pl>
2023-01-07 04:36:12 +0100

    Format the code inside the tests package

--- website/tests/conftest.py
+++ website/tests/conftest.py
@@ -1,16 +1,18 @@
 import pytest
-import sys 
+import sys
 import os
 from website.search_engine.ceneo import CeneoBrowser
 
 
 DEFAULT_PRODUCT_LIMIT_OPTION = 10
-DEFAULT_PRODUCT_SORT_OPTION = 'TRUE'
-DEFAULT_ALLEGRO_OPTION = 'FALSE'
+DEFAULT_PRODUCT_SORT_OPTION = "TRUE"
+DEFAULT_ALLEGRO_OPTION = "FALSE"
 DEFAULT_FILE_OPTION = "valid_products.txt"
 
 
 ceneo_browser = CeneoBrowser()
+
+
 @pytest.fixture(scope="class")
 def product_name_fixture(request):
     return request.param
@@ -18,37 +20,45 @@ def product_name_fixture(request):
 
 @pytest.fixture(scope="class")
 def product_list_fixture(pytestconfig, request):
-    if pytestconfig.getoption('allegro').upper() == DEFAULT_ALLEGRO_OPTION:
+    if pytestconfig.getoption("allegro").upper() == DEFAULT_ALLEGRO_OPTION:
         is_allegro_specific = True
     else:
         is_allegro_specific = False
 
     try:
-        limit = pytestconfig.getoption('limit')
+        limit = pytestconfig.getoption("limit")
     except ValueError:
-        sys.exit('ValueError: invalid literal for --limit')
+        sys.exit("ValueError: invalid literal for --limit")
 
-    if pytestconfig.getoption('sort').upper() == DEFAULT_PRODUCT_SORT_OPTION:
+    if pytestconfig.getoption("sort").upper() == DEFAULT_PRODUCT_SORT_OPTION:
         sort = True
     else:
         sort = False
     return ceneo_browser.search(request.param)
 
+
 def pytest_addoption(parser):
     parser.addoption("--file", action="store", default=DEFAULT_FILE_OPTION)
-    parser.addoption("--limit", action="store", default=DEFAULT_PRODUCT_LIMIT_OPTION)
-    parser.addoption("--sort", action="store", default=DEFAULT_PRODUCT_SORT_OPTION)
-    parser.addoption("--allegro", action="store", default=DEFAULT_ALLEGRO_OPTION)
+    parser.addoption(
+        "--limit", action="store", default=DEFAULT_PRODUCT_LIMIT_OPTION
+    )
+    parser.addoption(
+        "--sort", action="store", default=DEFAULT_PRODUCT_SORT_OPTION
+    )
+    parser.addoption(
+        "--allegro", action="store", default=DEFAULT_ALLEGRO_OPTION
+    )
+
 
 def get_product_names_tuple():
     script_dir = os.path.dirname(__file__)
-    path = os.path.join(script_dir, DEFAULT_FILE_OPTION) 
-    with open(path, 'r') as f:
-        product_names = f.read().split('\n')
-    product_names_tuples = list() 
+    path = os.path.join(script_dir, DEFAULT_FILE_OPTION)
+    with open(path, "r") as f:
+        product_names = f.read().split("\n")
+    product_names_tuples = list()
     for product_name in product_names:
         try:
-            if product_name[0] != '#':
+            if product_name[0] != "#":
                 product_names_tuples.append((product_name, product_name))
             else:
                 continue
--- website/tests/test_ceneobrowser.py
+++ website/tests/test_ceneobrowser.py
@@ -3,129 +3,155 @@ from .conftest import get_product_names_tuple
 
 
 @pytest.mark.parametrize(
-    'product_name_fixture, product_list_fixture',
+    "product_name_fixture, product_list_fixture",
     get_product_names_tuple(),
     indirect=True,
 )
 class TestCeneoSearch:
-
     def test_product_name(self, product_name_fixture, product_list_fixture):
-        for product in product_list_fixture: 
-            assert product.name is not None, \
-                f'[ "Search Query for "{product_name_fixture}" gives the Product '\
-                'object with no name ]'
+        for product in product_list_fixture:
+            assert product.name is not None, (
+                f'[ "Search Query for "{product_name_fixture}" gives the Product '
+                "object with no name ]"
+            )
 
     def test_product_url(self, product_name_fixture, product_list_fixture):
-        for product in product_list_fixture: 
-            assert product.url is not None, \
-                f'[ "Search Query for "{product_name_fixture}" gives the Product '\
-                f'object of name "{product.name}" with no ceneo offer'\
-                ' url ]'
-            assert re.match('https://www.ceneo.pl/[0-9]+', product.url),\
-                f'[ "Search Query for "{product_name_fixture}" gives the Product '\
-                f'object of name "{product.name}" with invalid ceneo '\
-                'offer url ]'
+        for product in product_list_fixture:
+            assert product.url is not None, (
+                f'[ "Search Query for "{product_name_fixture}" gives the Product '
+                f'object of name "{product.name}" with no ceneo offer'
+                " url ]"
+            )
+            assert re.match("https://www.ceneo.pl/[0-9]+", product.url), (
+                f'[ "Search Query for "{product_name_fixture}" gives the Product '
+                f'object of name "{product.name}" with invalid ceneo '
+                "offer url ]"
+            )
 
     def test_product_rating(self, product_name_fixture, product_list_fixture):
-        for product in product_list_fixture: 
+        for product in product_list_fixture:
             if product.rating is None:
                 assert True
             else:
-                assert product.rating >=0 and product.rating <= 5,\
-                    f'[ "Search Query for "{product_name_fixture}" gives the Product '\
-                    f'object of name "{product.name}" with invalid value '\
-                    'for rating ]'
+                assert product.rating >= 0 and product.rating <= 5, (
+                    f'[ "Search Query for "{product_name_fixture}" gives the Product '
+                    f'object of name "{product.name}" with invalid value '
+                    "for rating ]"
+                )
 
     def test_product_img_url(self, product_name_fixture, product_list_fixture):
-        for product in product_list_fixture: 
-            assert product.img is not None,\
-                f'[ "Search Query for "{product_name_fixture}" gives the Product '\
-                f'object of name "{product.name}" with no ceneo img url ]'
-            assert re.match(
-                        'https://image.ceneostatic.pl/data/products/[0-9]+/.*', 
-                        product.img
-                        ),\
-                f'[ "Search Query for "{product_name_fixture}" gives the Product '\
-                f'object of name "{product.name}" with invalid ceneo '\
-                'img url ]'
-
-    def test_product_description(self, product_name_fixture, product_list_fixture):
-        for product in product_list_fixture: 
-            assert isinstance(product.description, str), \
-                f'[ "Search Query for "{product_name_fixture}" gives the Product '\
-                f'object of name "{product.name}" with invalid product '\
-                'description ]'
-
-    def test_product_shop_list(self, product_name_fixture, product_list_fixture):
         for product in product_list_fixture:
-            assert product.shop_list, \
-                f'[ "Search Query for "{product_name_fixture}" gives the Product '\
+            assert product.img is not None, (
+                f'[ "Search Query for "{product_name_fixture}" gives the Product '
+                f'object of name "{product.name}" with no ceneo img url ]'
+            )
+            assert re.match(
+                "https://image.ceneostatic.pl/data/products/[0-9]+/.*",
+                product.img,
+            ), (
+                f'[ "Search Query for "{product_name_fixture}" gives the Product '
+                f'object of name "{product.name}" with invalid ceneo '
+                "img url ]"
+            )
+
+    def test_product_description(
+        self, product_name_fixture, product_list_fixture
+    ):
+        for product in product_list_fixture:
+            assert isinstance(product.description, str), (
+                f'[ "Search Query for "{product_name_fixture}" gives the Product '
+                f'object of name "{product.name}" with invalid product '
+                "description ]"
+            )
+
+    def test_product_shop_list(
+        self, product_name_fixture, product_list_fixture
+    ):
+        for product in product_list_fixture:
+            assert product.shop_list, (
+                f'[ "Search Query for "{product_name_fixture}" gives the Product '
                 f'object of name "{product.name}" with no shop list ]'
+            )
 
     def test_shop_names(self, product_name_fixture, product_list_fixture):
         for product in product_list_fixture:
             for shop in product.shop_list:
-                assert shop.name is not None, \
-                    f'[ "Search Query for "{product_name_fixture}" gives the Product '\
+                assert shop.name is not None, (
+                    f'[ "Search Query for "{product_name_fixture}" gives the Product '
                     f'object of name "{product.name}" with shop of no name ]'
+                )
 
     def test_shop_price(self, product_name_fixture, product_list_fixture):
         for product in product_list_fixture:
             for shop in product.shop_list:
-                assert shop.price is not None, \
-                    f'[ "Search Query for "{product_name_fixture}" gives the Product '\
-                    f'object of name "{product.name}" with shop offer of'\
+                assert shop.price is not None, (
+                    f'[ "Search Query for "{product_name_fixture}" gives the Product '
+                    f'object of name "{product.name}" with shop offer of'
                     f' "{shop.name}" with no price ]'
-                assert shop.price > 0, \
-                    f'[ "Search Query for "{product_name_fixture}" gives the Product '\
-                    f'object of name "{product.name}" with shop offer of'\
+                )
+                assert shop.price > 0, (
+                    f'[ "Search Query for "{product_name_fixture}" gives the Product '
+                    f'object of name "{product.name}" with shop offer of'
                     f' "{shop.name}" with nonpositive price ]'
-                decimal_position = decimal.Decimal(
-                                                str(shop.price)
-                                                ).as_tuple().exponent
-                assert decimal_position == -1 or decimal_position == -2,\
-                    f'[ "Search Query for "{product_name_fixture}" gives the Product '\
-                    f'object of name "{product.name}" with shop offer of'\
+                )
+                decimal_position = (
+                    decimal.Decimal(str(shop.price)).as_tuple().exponent
+                )
+                assert decimal_position == -1 or decimal_position == -2, (
+                    f'[ "Search Query for "{product_name_fixture}" gives the Product '
+                    f'object of name "{product.name}" with shop offer of'
                     f' "{shop.name}" with invalid price format ]'
+                )
 
     def test_shop_url(self, product_name_fixture, product_list_fixture):
         for product in product_list_fixture:
             for shop in product.shop_list:
-                assert re.match('https://www.ceneo.pl/.*', shop.url), \
-                    f'[ "Search Query for "{product_name_fixture}" gives the Product '\
-                    f'object of name "{product.name}" with shop offer of'\
+                assert re.match("https://www.ceneo.pl/.*", shop.url), (
+                    f'[ "Search Query for "{product_name_fixture}" gives the Product '
+                    f'object of name "{product.name}" with shop offer of'
                     f' "{shop.name}" with invalid url ]'
+                )
 
-    def test_shop_delivery_price(self, product_name_fixture, product_list_fixture):
+    def test_shop_delivery_price(
+        self, product_name_fixture, product_list_fixture
+    ):
         for product in product_list_fixture:
             for shop in product.shop_list:
-                assert shop.delivery_price is not None, \
-                    f'[ "Search Query for "{product_name_fixture}" gives the Product '\
-                    f'object of name "{product.name}" with shop offer of'\
+                assert shop.delivery_price is not None, (
+                    f'[ "Search Query for "{product_name_fixture}" gives the Product '
+                    f'object of name "{product.name}" with shop offer of'
                     f' "{shop.name}" with no delivery price ]'
+                )
 
-    def test_shop_availability(self, product_name_fixture, product_list_fixture):
+    def test_shop_availability(
+        self, product_name_fixture, product_list_fixture
+    ):
         for product in product_list_fixture:
             for shop in product.shop_list:
-                assert shop.availability is not None, \
-                    f'[ "Search Query for "{product_name_fixture}" gives the Product '\
-                    f'object of name "{product.name}" with shop offer of'\
+                assert shop.availability is not None, (
+                    f'[ "Search Query for "{product_name_fixture}" gives the Product '
+                    f'object of name "{product.name}" with shop offer of'
                     f' "{shop.name}" with no availability info ]'
+                )
 
-    def test_shop_delivery_time(self, product_name_fixture, product_list_fixture):
+    def test_shop_delivery_time(
+        self, product_name_fixture, product_list_fixture
+    ):
         for product in product_list_fixture:
             for shop in product.shop_list:
-                assert shop.delivery_time is not None, \
-                    f'[ "Search Query for "{product_name_fixture}" gives the Product '\
-                    f'object of name "{product.name}" with shop offer of'\
+                assert shop.delivery_time is not None, (
+                    f'[ "Search Query for "{product_name_fixture}" gives the Product '
+                    f'object of name "{product.name}" with shop offer of'
                     f' "{shop.name}" with no delivery time info ]'
+                )
 
     def test_price_sort(self, product_name_fixture, product_list_fixture):
         prev_offer_price = 0
         for product in product_list_fixture:
             next_offer_price = min(shop.price for shop in product.shop_list)
-            assert prev_offer_price <= next_offer_price, \
-                f'[ "Search Query for "{product_name_fixture}" gives the Product '\
-                f'object of name "{product.name}" with not working price '\
-                'sorting ]'
+            assert prev_offer_price <= next_offer_price, (
+                f'[ "Search Query for "{product_name_fixture}" gives the Product '
+                f'object of name "{product.name}" with not working price '
+                "sorting ]"
+            )
             prev_offer_price = next_offer_price
